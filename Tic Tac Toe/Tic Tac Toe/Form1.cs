e8b0e243666df3f4f8600296147ea72e8ce43000
mlogic1 <multylogic@gmail.com>
2014-12-23 13:16:59 +0100

    Commit

---
+++ Tic Tac Toe/Tic Tac Toe/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        GameGrid g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playerGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = new GameGrid(label_result);
+            g.Location = new Point(10, 35);
+            this.Controls.Add(g);
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.Dispose();
+            label_result.Visible = false;
+            g = new GameGrid(label_result);
+            g.Location = new Point(10, 35);
+            this.Controls.Add(g);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+    }
+}
---
+++ Tic Tac Toe/Tic Tac Toe/GameGrid.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Tic_Tac_Toe
+{
+    class GameGrid:Panel
+    {
+
+        //private Color brushColor; later for custom grid paint
+
+        Bitmap CurrentTurn;
+        PictureBox[] Zones = new PictureBox[9];
+        Boolean[] IsZoneTaken = new Boolean[9];
+        Label result;
+
+        //constructor
+        public GameGrid(Label l_result)
+        {
+            this.Paint += new PaintEventHandler(DrawGrid);
+
+            result = l_result;
+            BackColor = SystemColors.Control;
+            this.Size = new Size(500, 500);
+            SetZones();
+
+        }
+
+
+        private void DrawGrid(object sender, PaintEventArgs e)
+        {
+            var p = sender as Panel;
+            var g = e.Graphics;
+
+            //Create a brush
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            //Draw the game grid
+            g.FillRectangle(brush, 175, 25, 10, 450);
+            g.FillRectangle(brush, 325, 25, 10, 450);
+            g.FillRectangle(brush, 25, 175, 450, 10);
+            g.FillRectangle(brush, 25, 325, 450, 10);
+
+        }
+
+
+
+        private void SetZones()
+        {
+            
+            //Set First Turn to be X
+            CurrentTurn = Tic_Tac_Toe.Properties.Resources.X;
+
+            //Create and set each Zone's size, name declare their event handlers
+            for (int i = 0; i < 9;i++ )
+            {
+                Zones[i] = new PictureBox();
+                Zones[i].Size = new Size(125, 125);
+                Zones[i].Name = "z" + i;
+                
+                IsZoneTaken[i] = new Boolean();
+                IsZoneTaken[i] = false;
+
+                Zones[i].MouseEnter += new EventHandler(ZoneMouseEnter);
+                Zones[i].MouseLeave += new EventHandler(ZoneMouseLeave);
+                Zones[i].MouseClick += new MouseEventHandler(ZoneMouseClick);
+            }
+
+           
+
+            //Set Zone location individually
+            Zones[0].Location = new Point(35, 35);
+            Zones[1].Location = new Point(195, 35);
+            Zones[2].Location = new Point(345, 35);
+            Zones[3].Location = new Point(35, 195);
+            Zones[4].Location = new Point(195, 195);
+            Zones[5].Location = new Point(345, 195);
+            Zones[6].Location = new Point(35, 345);
+            Zones[7].Location = new Point(195, 345);
+            Zones[8].Location = new Point(340, 340);
+           
+            //Add zones to game panel
+            foreach (PictureBox p in Zones)
+            {
+                this.Controls.Add(p);
+            }
+        }
+
+
+        private void ZoneMouseEnter(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+
+            //Checks if there's already a symbol in that specific zone, if there isnt, draws the current player's symbol
+            if (IsZoneTaken[Convert.ToInt32(p.Name.Substring(1,1))])
+            {
+            }
+            else
+            {
+                p.Image = CurrentTurn;
+            }
+            
+        }
+
+        private void ZoneMouseLeave(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+
+            //If there was not a symbol inside the zone on mouse leave remove the image
+            if (IsZoneTaken[Convert.ToInt32(p.Name.Substring(1, 1))])
+            {
+            }
+            else
+            {
+                p.Image = null;
+            }
+
+        }
+
+        private void ZoneMouseClick(object sender, MouseEventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+
+            /*Checks if there's already a symbol in that specific zone, if there isnt, draws the current
+             player's symbol permanently, sets the zone to be taken and switches the player */
+            if (!IsZoneTaken[Convert.ToInt32(p.Name.Substring(1, 1))])
+            {
+                p.Image = CurrentTurn;
+                
+                //check if current turn was X or Y and sets the tag accordingly
+                if (CurrentTurn.GetPixel(62, 62) == Tic_Tac_Toe.Properties.Resources.X.GetPixel(62, 62))
+                {
+                    p.Tag = "X";
+                }
+                else
+                {
+                    p.Tag = "Y";
+                }
+                //Check for victory on each successful symbol add and play sound
+                PlayClickSound();
+                CheckForVictory();
+                IsZoneTaken[Convert.ToInt32(p.Name.Substring(1, 1))] = true;
+                SwitchTurn();
+            }
+            else
+            {
+            }
+            
+
+        }
+
+        
+
+        //Switches X and O
+        private void SwitchTurn()
+        {
+            if (CurrentTurn.GetPixel(62,62) == Tic_Tac_Toe.Properties.Resources.X.GetPixel(62,62))
+            {
+                  CurrentTurn = new Bitmap(Tic_Tac_Toe.Properties.Resources.O);
+            }
+            else
+            {
+                CurrentTurn = Tic_Tac_Toe.Properties.Resources.X;
+            }
+        }
+
+
+        private void CheckForVictory()
+        {
+
+            //tie check
+            if (IsZoneTaken[0] == true && IsZoneTaken[1] == true && IsZoneTaken[2] == true && IsZoneTaken[3] == true && IsZoneTaken[4] == true && IsZoneTaken[5] == true && IsZoneTaken[6] == true && IsZoneTaken[7] == true && IsZoneTaken[8] == true)
+            {
+                result.Visible = true; // ne radi
+            }
+
+
+            //linear check
+            if (Zones[0].Tag == Zones[1].Tag && Zones[1].Tag != null)
+            {
+                if (Zones[1].Tag == Zones[2].Tag)
+                {
+                    result.Visible = true;
+                    PlayVictorySound();
+                }
+            }
+
+            if (Zones[3].Tag == Zones[4].Tag && Zones[4].Tag != null)
+            {
+                if (Zones[4].Tag == Zones[5].Tag)
+                {
+                    result.Visible = true;
+                    PlayVictorySound();
+                }
+            }
+
+            if (Zones[6].Tag == Zones[7].Tag && Zones[7].Tag != null)
+            {
+                if (Zones[7].Tag == Zones[8].Tag)
+                {
+                    result.Visible = true;
+                    PlayVictorySound();
+                }
+            }
+
+            //diagonal check
+            if (Zones[0].Tag == Zones[4].Tag && Zones[4].Tag != null)
+            {
+                if (Zones[4].Tag == Zones[8].Tag)
+                {
+                    result.Visible = true;
+                    PlayVictorySound();
+                }
+            }
+
+            if (Zones[6].Tag == Zones[4].Tag && Zones[4].Tag != null)
+            {
+                if (Zones[4].Tag == Zones[2].Tag)
+                {
+                    result.Visible = true;
+                    PlayVictorySound();
+                }
+            }
+
+        }
+
+        private void PlayVictorySound()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Tic_Tac_Toe.Properties.Resources.victory);
+            player.Play();
+        }
+
+        private void PlayClickSound()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Tic_Tac_Toe.Properties.Resources.click);
+            player.Play();
+        }
+        
+        
+    }
+}
